f455963393e17a351a363d6a2b20eaa2c38fb745
root <root@arch-kory2.bullardisd.net>
2014-04-17 13:48:05 -0500

    remove kernel deb from rootfs

--- clean.sh
+++ clean.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
 pushd root
-rm -Rf vmlinuz*
-rm -Rf initrd*
-rm -Rf boot/vmlinuz*
-rm -Rf boot/initrd*
+rm vmlinuz*
+rm initrd*
+rm boot/vmlinuz*
+rm boot/initrd*
+rm linux-image*
 
-rm -Rf var/lib/apt/lists/archive*
-rm -Rf var/cache/apt/pkgcache.bin
-rm -Rf var/cache/apt/srcpkgcache.bin
-rm -Rf var/cache/apt/archives/*.deb
+
+rm var/lib/apt/lists/archive*
+rm var/cache/apt/pkgcache.bin
+rm var/cache/apt/srcpkgcache.bin
+rm var/cache/apt/archives/*.deb
 rm var/cache/apt/archives/partial/*
 
 popd
